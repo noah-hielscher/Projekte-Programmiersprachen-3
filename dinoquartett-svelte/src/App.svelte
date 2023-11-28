4d84f066c4b89607ce5b65d7dba39b2fb3a132ac
Noah Hielscher <noah.hielscher@hfg.design>
2023-11-28 16:03:57 +0100

    svelte Card

--- dinoquartett-svelte/src/App.svelte
+++ dinoquartett-svelte/src/App.svelte
@@ -1,47 +1,51 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+	import Card from "./Card.svelte";
+	import dinosaurs from "./dinosaurs_data.js";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<header>
+			<logo
+				><img id="logo" src="./img/Logo.png" alt="Logo von Qartett" />
+			</logo>
+			<nav>
+				<h3 class="navi">Startseite</h3>
+				<h3 class="navi">Sortierung</h3>
+			</nav>
+		</header>
+		<main>
+			<div id="layout">
+				<div id="text">
+					<h1>Dinosaurier aus der Geschichte!</h1>
+					<p>
+						Tauchen Sie ein in die faszinierende Welt der
+						<a
+							href="https://de.wikipedia.org/wiki/Dinosaurier"
+							target="_blank"
+							>Dinosaurier!</a
+						>
+						Vor Millionen von Jahren bevölkerten diese
+						majestätischen Kreaturen unseren Planeten. Vom
+						imposanten Brachiosaurus bis zum furchterregenden
+						Tyrannosaurus Rex - erleben Sie die spannende Geschichte
+						dieser prähistorischen Giganten. Gemeinsam begeben wir
+						uns auf eine musikalische Reise durch die Ära der
+						Dinosaurier, erkundend, wie ihre einzigartigen
+						Lebenswelten durch die Klänge eines Quartetts zum Leben
+						erwachen.
+					</p>
+				</div>
+				<div id="container">
+					{#each dinosaurs as dinosaur}
+					<Card dino={dinosaur} />
+					{/each}
+				</div>
+			</div>
+		</main>
+		<footer>
+			<p>Diese Webseite wurde erstellt von</p>
+			<img id="dino" src="./img/cursor-animat.gif" alt="Dino" />
+		</footer>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+ 
 </style>
---
+++ dinoquartett-svelte/src/Card.svelte
@@ -0,0 +1,85 @@
+<script>
+  // wir brauchen einen Prop zum erstellen einer Karte
+  export let dino;
+
+  //img Pfad
+  const imgPfad = "../public/Dino-img/"+dino.group_letter + dino.card_number+ "_" + dino.name + ".png";
+  console.log(imgPfad)
+
+</script>
+
+
+		 <div id="wrapper">
+  <h2 style="background-color: rgb(243, 158, 189);">{dino.group_letter}</h2>
+  <div class="description">
+    <h3>{dino.name}</h3>
+    <h5>{dino.group}</h5>
+  </div>
+  <img id="image" src="{imgPfad}" alt="Brachiosaurus">
+  <article id="wrapper2">
+    <section class="card topLeftRounded" id="karte-1">
+      <img class="icon" src="./img/years.png" alt="BrachiosaurusStats-icons">
+     
+    </section>
+    <section class="card" id="karte-2">
+       <div class="cardText">
+        <h5>years</h5>
+        <h4>{dino.years}</h4>
+      </div>
+    </section>
+    <section class="card" id="karte-3">
+       <img class="icon" src="./img/speed.png" alt="BrachiosaurusStats-icons">
+    </section>
+    <section class="card topRightRounded" id="karte-4">
+     
+      <div class="cardText">
+        <h5>speed</h5>
+        <h4>{dino.speed}</h4>
+      </div>
+    </section>
+    <section class="card" id="karte-5">
+       <img class="icon" src="./img/height.png" alt="BrachiosaurusStats-icons">
+    </section>
+    <section class="card" id="karte-6">
+      <div class="cardText">
+        <h5>height</h5>
+        <h4>{dino.height}</h4>
+      </div>
+     
+    </section>
+    <section class="card" id="karte-7">
+       <img class="icon" src="./img/teeth.png" alt="BrachiosaurusStats-icons">
+    </section>
+    <section class="card" id="karte-8">
+      <div class="cardText">
+        <h5>teeth</h5>
+        <h4>{dino.number_teeth}</h4>
+      </div>
+     
+    </section>
+    <section class="card bottomLeftRounded" id="karte-9">
+      <img class="icon" src="./img/iq.png" alt="BrachiosaurusStats-icons">
+    </section>
+    <section class="card" id="karte-10">
+      <div class="cardText">
+        <h5>iq</h5>
+        <h4>{dino.iq}</h4>
+      </div>
+      
+    </section>
+    <section class="card" id="karte-11">
+      <img class="icon" src="./img/eggs.png" alt="BrachiosaurusStats-icons">
+    </section>
+    <section class="card bottomRightRounded" id="karte-12">
+      <div class="cardText">
+        <h5>eggs</h5>
+        <h4>{dino.number_eggs}</h4>
+      </div>
+      
+    </section>
+  </article>
+</div>
+
+
+<style>
+</style>
